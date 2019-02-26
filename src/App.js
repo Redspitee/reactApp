8c7dcb8561f492790402e7f2cb37221b86198181
redspite <718352984@qq.com>
2019-02-26 13:53:07 +0800

    0226

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { connect } from 'react-redux';
+import { Button } from 'antd';
+import { ADD_ACTION, REMOVE_ACTION, ADD_ASYNC_ACTION } from './index.redux';
+@connect(
+  state => ({ num: state }),
+  {ADD_ACTION,REMOVE_ACTION,ADD_ASYNC_ACTION}
+)
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       {this.props.num}
+        <Button onClick={this.props.ADD_ACTION}>num+1</Button>
       </div>
     );
   }
